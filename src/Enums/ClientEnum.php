264176aa388809f34d394e42d7a0e906fc235257
yaozhiwen <929089994@qq.com>
2022-02-17 13:48:25 +0800

    修改代码

--- src/Enums/ClientEnum.php
+++ src/Enums/ClientEnum.php
@@ -14,16 +14,10 @@ namespace Kabel\Sign\Enums;
 class ClientEnum
 {
     // 客户端枚举
-    const ACCOUNT = 1;
-    const MANAGER = 2;
-    const MASTER = 3;
-    const FRAMEWORK = 4;
-    public static array $clientMap = [
-        self::ACCOUNT => 'account',
-        self::MANAGER => 'manager',
-        self::MASTER => 'master',
-        self::FRAMEWORK => 'framework',
-    ];
+    const ACCOUNT = 'account';
+    const MANAGER = 'manager';
+    const MASTER = 'master';
+    const FRAMEWORK = 'framework';
     /**
      * @param  int  $enumType
      * @return array
